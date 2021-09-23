af59e8236186aa9c05a97cc5cd0a5bebaeefa51e
lorenabazantorres <lorenabazantorres@gmail.com>
2021-09-23 10:13:03 +0200

    Byte

---
+++ TipoByte/src/tipobyte/TipoByte.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tipobyte;
+
+/**
+ *
+ * @author LORENA
+ */
+public class TipoByte {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        byte num = 2;
+        System.out.println("El numero es: " + num);
+        System.out.println("El valor mínimo es: " + Byte.MIN_VALUE);
+        System.out.println("El valor máximo es: " + Byte.MAX_VALUE);
+    }
+
+}
